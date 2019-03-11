eb92c4b2aed2658fa509004966c160935669a498
Garima Kapila <admin@Garimas-MacBook-Air.local>
2019-03-11 00:06:51 -0700

    Updates

---
+++ precache-manifest.5f48b827a29f21573987d7936707775c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e65c2a614497db286ecaefbb95df1db",
+    "url": "/static/media/SunnyvaleInterior.9e65c2a6.png"
+  },
+  {
+    "revision": "e7513148cf3cde2bc373",
+    "url": "/static/css/main.be3c5237.chunk.css"
+  },
+  {
+    "revision": "519841109c8fc70fef24",
+    "url": "/static/js/1.51984110.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2117bc3705d4e945cfb2fdf1210f2428",
+    "url": "/static/media/SunnyvaleExterior.2117bc37.png"
+  },
+  {
+    "revision": "371953d78642671ee96ccd03d0a37f8c",
+    "url": "/static/media/CupertinoExterior.371953d7.png"
+  },
+  {
+    "revision": "e7513148cf3cde2bc373",
+    "url": "/static/js/main.e7513148.chunk.js"
+  },
+  {
+    "revision": "d70b297bb81ba4f6af0af7bd07c6db78",
+    "url": "/static/media/CupertinoInterior.d70b297b.png"
+  },
+  {
+    "revision": "bc6d5606dc9e166c2df135ae199ee0f7",
+    "url": "/static/media/SunnyvaleStreet.bc6d5606.png"
+  },
+  {
+    "revision": "a4ce8262c8cf6e13e20a3b56232e9938",
+    "url": "/static/media/CupertinoStreet.a4ce8262.png"
+  },
+  {
+    "revision": "fe500fce42aa2cd46908b2bca687c175",
+    "url": "/static/media/DrKavitaKapila.fe500fce.png"
+  },
+  {
+    "revision": "3a6dd53618ccc9fe23c5a38ddcd93347",
+    "url": "/index.html"
+  }
+];
